41bda2d22660c3353628bca5beabbafebc3abfc4
nithamitabh <21dec004@nith.ac.in>
2023-01-28 13:33:53 +0530

    second commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
 import Image from '../assets/about.png'
-
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
+import { TypeAnimation } from 'react-type-animation'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../variant'
 
 const Banner = () => {
   return (
-    <div className='section' id='home'>
-      
-    </div>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+      <div className="container mx-auto">
+        <div className='flex flex-col gap-y-8 lg:items-center lg:gap-x-12 lg:flex-row'>
+          <div>
+            <div className='flex-1 text-center font-secondary lg:text-left'><motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[80px]'>Amitabh <span>Kumar</span></motion.h1></div>
+            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold text-center uppercase leading - [1] lg:text-left'><span className='mr-4 text-white'>I am a</span>
+              <TypeAnimation sequence={[
+                'Web Developer',
+                2000,
+                'App Developer',
+                2000,
+                'UI Designer',
+                2000,
+                'Student',
+                2000,
+                'Freelancer',
+                2000
+              ]} speed={50} className='text-accent' wrapper='span' repeat={Infinity} /></div>
+            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dignissimos voluptas, culpa harum commodi tem</p>
+            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'><button className='btn btn-lg'>Contact me</button>
+              <a href="#" className='text-gradient btn-link'>My Portfolio</a>
+            </div>
+            {/* Social Icons */}
+            <div className='flex text-20 gap-x-6 max-w-max mx-auto lg:mx-0'>
+              <a href="https://github.com/nithamitabh"><FaGithub /></a>
+              <a href="https://www.linkedin.com/in/amitabh-kumar-392671231/"><FaLinkedin /></a>
+              <a href="https://twitter.com/amitabh85467656"><FaTwitter /></a>
+              <a href="https://www.instagram.com/amitabhkumarharsh/"><FaInstagram /></a>
+            </div>
+          </div>
+          <div className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'> <img src={Image} alt="" /></div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ const Header = () => {
   return (
     <header className='py-8'>
       <div className="container mx-auto">
-        <div className='flex justify-between items-center0'>
-        <a href="#"><h1 className='font-bold capitalize text-3xl text-blue-600'>AMITABH</h1> <h2 className='font-bold text-white text-2xl '>Kumar</h2> </a>
+        <div className='flex justify-between items-center'>
+        <a href="#"><h1 className='font-bold capitalize text-2xl text-blue-600'>AMITABH</h1> <h2 className='font-bold text-white text-xl  lg:mb-10 '>Kumar</h2> </a>
         <button className='btn btn-sm'>Hire Me</button>
         </div>
       </div>
