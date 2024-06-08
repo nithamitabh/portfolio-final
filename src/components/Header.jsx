adc9fe1fa92ff22d53e83a16e2b2765f038fdc79
nithamitabh <21dec004@nith.ac.in>
2024-06-08 11:27:41 +0530

    update packages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     <header className='py-8'>
       <div className="container mx-auto">
         <div className='flex justify-between items-center'>
-        <a href="#"><h1 className='font-bold capitalize text-2xl text-blue-600'>AMITABH</h1> <h2 className='font-bold text-white text-xl  lg:mb-10 '>Kumar</h2> </a>
+        <a href="/"><h1 className='font-bold capitalize text-2xl text-blue-600'>AMITABH</h1> <h2 className='font-bold text-white text-xl  lg:mb-10 '>Kumar</h2> </a>
         <a href="https://www.linkedin.com/in/amitabh-kumar-392671231/"><button className='btn btn-sm'>Hire Me</button></a>
         </div>
       </div>
